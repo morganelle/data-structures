2c2d0d793e9cdf2c1d248025d34720ac100bb7cf
Morgan Nomura <morganelle@gmail.com>
2017-07-05 10:50:21 -0700

    updated insert methods and max depth and left right balances

--- src/test_bst.py
+++ src/test_bst.py
@@ -92,32 +92,38 @@ def test_init_list_float():
     new_tree = BinarySearchTree(2.5)
     assert new_tree._root._data == 2.5
     assert new_tree._size == 1
-    assert new_tree._rbal == 0
-    assert new_tree._lbal == 0
-    assert new_tree._max_depth == 0
+    assert new_tree._rbal == 1
+    assert new_tree._lbal == 1
+    assert new_tree._max_depth == 1
 
 
 def test_init_list():
     """Test init with iterable."""
     new_tree = BinarySearchTree([2, 1, 3])
     assert new_tree._size == 3
-    assert new_tree._rbal == 1
-    assert new_tree._lbal == 1
-    assert new_tree._max_depth == 1
+    assert new_tree._rbal == 2
+    assert new_tree._lbal == 2
+    assert new_tree._max_depth == 2
 
 
 def test_init_tuple():
     """Test init with iterable."""
     new_tree = BinarySearchTree((2, 1, 3))
     assert new_tree._size == 3
-    assert new_tree._rbal == 1
-    assert new_tree._lbal == 1
-    assert new_tree._max_depth == 1
+    assert new_tree._rbal == 2
+    assert new_tree._lbal == 2
+    assert new_tree._max_depth == 2
 
 
-def test_size():
+def test_size_on_iterable():
     """Test size."""
-    new_tree = BinarySearchTree()
+    new_tree = BinarySearchTree((2, 1, 3))
+    assert new_tree.size() == 3
+
+
+def test_size_on_inserts(empty_tree):
+    """Test size after inserts."""
+    new_tree = empty_tree
     assert new_tree.size() == 0
     new_tree.insert(2)
     assert new_tree.size() == 1
@@ -127,8 +133,8 @@ def test_size():
     assert new_tree.size() == 3
 
 
-def test_balance():
-    """Test balance."""
+def test_balance_on_inserts():
+    """Test balance after inserts."""
     new_tree = BinarySearchTree()
     assert new_tree.balance() == 0
     new_tree.insert(2)
@@ -141,49 +147,50 @@ def test_balance():
     assert new_tree.balance() == 1
 
 
-def test_depth():
-    """Test depth."""
+def test_depth_on_inserts():
+    """Test depth after inserts."""
     new_tree = BinarySearchTree()
     assert new_tree.depth() == 0
     new_tree.insert(2)
-    assert new_tree.depth() == 0
+    assert new_tree.depth() == 1
     new_tree.insert(1)
-    assert new_tree.depth() == 1
-    new_tree.insert(3)
-    assert new_tree.depth() == 1
-    new_tree.insert(4)
     assert new_tree.depth() == 2
+    new_tree.insert(3)
+    assert new_tree.depth() == 2
+    new_tree.insert(4)
+    assert new_tree.depth() == 3
 
 
-def test_search():
+def test_search_empty_tree():
+    """Test search."""
+    new_tree = BinarySearchTree()
+    assert new_tree.search(2) is None
+
+
+def test_search_after_insert():
     """Test search."""
     new_tree = BinarySearchTree()
     assert new_tree.search(2) is None
     new_tree.insert(2)
     assert new_tree.search(2)._data == 2
-    new_tree.insert(1)
-    assert new_tree.search(1)._data == 1
-    new_tree.insert(3)
-    assert new_tree.search(4) is None
-    new_tree.insert(4)
-    assert new_tree.search(4)._data == 4
+
+
+def test_search_string(empty_tree):
+    """Searching for a string raises Value Error."""
+    with pytest.raises(TypeError):
+        empty_tree.search('2')
 
 
 def test_contains():
     """Test contains method."""
-    new_tree = BinarySearchTree()
-    assert new_tree.contains(2) is False
-    new_tree.insert(2)
+    new_tree = BinarySearchTree([2, 1, 3, 4])
     assert new_tree.contains(2) is True
-    new_tree.insert(1)
     assert new_tree.contains(1) is True
-    new_tree.insert(3)
-    assert new_tree.contains(4) is False
-    new_tree.insert(4)
+    assert new_tree.contains(10) is False
     assert new_tree.contains(4) is True
 
 
-def test_insert_tuple(empty_tree):
+def test_insert_invalid_tuple(empty_tree):
     """Test inserting tuple raises error."""
     with pytest.raises(TypeError):
         empty_tree.insert((9, 'hello'))
@@ -196,9 +203,9 @@ def test_insert_dupe(empty_tree):
         empty_tree.insert(2)
 
 
-def test_insert():
+def test_contains_multiple_inserts(empty_tree):
     """Test insert."""
-    new_tree = BinarySearchTree()
+    new_tree = empty_tree
     assert new_tree.contains(2) is False
     new_tree.insert(2)
     assert new_tree.contains(2) is True
@@ -213,7 +220,7 @@ def test_insert():
 def test_insert_depth(tree_init_three_nodes_left):
     """Test insert."""
     tree_init_three_nodes_left.delete(7)
-    assert tree_init_three_nodes_left._max_depth is 2
+    assert tree_init_three_nodes_left._max_depth is 3
 
 
 def test_breadth_first(tree_init_list):
@@ -281,8 +288,8 @@ def test_del_node_left_kid(tree_init_three_nodes_left):
     tree_init_three_nodes_left.delete(6)
     assert tree_init_three_nodes_left._root._data == 4
     assert tree_init_three_nodes_left._root._rkid._data == 5
-    assert tree_init_three_nodes_left.depth() == 1
-    assert tree_init_three_nodes_left._max_depth == 1
+    assert tree_init_three_nodes_left.depth() == 2
+    assert tree_init_three_nodes_left._max_depth == 2
 
 
 def test_del_root_one_kid_right(tree_init_one_node_right):
@@ -408,33 +415,33 @@ def test_del_left_branch(tree_init_list):
 
 def test_del_rebalance(tree_init_list):
     """Test rebalance after delete."""
-    assert tree_init_list._rbal == 2
-    assert tree_init_list._lbal == 2
-    assert tree_init_list._max_depth == 2
+    assert tree_init_list._rbal == 3
+    assert tree_init_list._lbal == 3
+    assert tree_init_list._max_depth == 3
     tree_init_list.delete(4)
-    assert tree_init_list._rbal == 2
-    assert tree_init_list._lbal == 2
-    assert tree_init_list._max_depth == 2
+    assert tree_init_list._rbal == 3
+    assert tree_init_list._lbal == 3
+    assert tree_init_list._max_depth == 3
     tree_init_list.delete(7)
+    assert tree_init_list._rbal == 2
+    assert tree_init_list._lbal == 3
+    assert tree_init_list._max_depth == 3
+    tree_init_list.delete(6)
+    assert tree_init_list._rbal == 1
+    assert tree_init_list._lbal == 3
+    assert tree_init_list._max_depth == 3
+    tree_init_list.delete(1)
+    tree_init_list.delete(3)
     assert tree_init_list._rbal == 1
     assert tree_init_list._lbal == 2
     assert tree_init_list._max_depth == 2
-    tree_init_list.delete(6)
-    assert tree_init_list._rbal == 0
-    assert tree_init_list._lbal == 2
-    assert tree_init_list._max_depth == 2
-    tree_init_list.delete(1)
-    tree_init_list.delete(3)
-    assert tree_init_list._rbal == 0
+    tree_init_list.delete(2)
+    assert tree_init_list._rbal == 1
     assert tree_init_list._lbal == 1
     assert tree_init_list._max_depth == 1
-    tree_init_list.delete(2)
-    assert tree_init_list._rbal == 0
-    assert tree_init_list._lbal == 0
-    assert tree_init_list._max_depth == 0
 
 
 def test_del_rebalance_right(tree_init_three_nodes_right):
     """Test max depth on right-imbalanced tree."""
     tree_init_three_nodes_right.delete(6)
-    assert tree_init_three_nodes_right._max_depth is 1
+    assert tree_init_three_nodes_right._max_depth is 2
